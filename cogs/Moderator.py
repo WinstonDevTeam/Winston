3c7964250985cd16bb1e9f3ba14c7a126d8efe0c
Scandlex <63729658+Scandlex@users.noreply.github.com>
2021-01-06 10:38:26 +0400

    moderator cog

---
+++ cogs/Moderator.py
@@ -0,0 +1,67 @@
+import discord
+from discord.ext import commands
+
+#Moderator Commands
+
+class Moderator(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(manage_messages = True, administrator = True)
+    async def clear(ctx, amount : int):
+        await ctx.channel.purge(limit = amount)
+        await ctx.send(f"{amount} message's have been cleared!")
+
+    @clear.error
+    async def clear_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specify a amount of messages to delete.")
+
+    @commands.command()
+    @commands.has_permissions(kick_members = True, administrator = True)
+    async def kick(ctx, member : discord.Member, *, reason = None):
+        await member.kick(reason = reason)
+        await ctx.send(f"{member} has been kicked for {reason}.")
+
+    @kick.error
+    async def kick_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specify a user.")
+
+    @commands.command() 
+    @commands.has_permissions(ban_members = True, administrator = True)
+    async def ban(ctx, member: discord.Member, *, reason = None):
+        await member.ban(reason = reason)
+        await ctx.send(f"{member} has been banned for {reason}.")
+
+    @ban.error
+    async def ban_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specify a user.")
+
+    @commands.command()
+    @commands.has_permissions(ban_members = True, administrator = True)
+    async def unban(ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split("#")
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+        
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"{member_name}#{member_discriminator} has been unbanned.")
+                return
+            else:
+                await ctx.send("User is not banned.")
+
+    @unban.error
+    async def unban_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specify a user.")
+
+
+def setup(client):
+    client.add_cog(Moderator(client))
