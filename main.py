f970ce0596ae35169d00da993cbffc92e0aea133
Scandlex <63729658+Scandlex@users.noreply.github.com>
2021-01-23 13:56:05 +0400

    added avatar and guildinfo

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from itertools import cycle
 #Intents
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix = ".", intents = intents)
+client = commands.Bot(command_prefix = "/", intents = intents)
 
-status_a = cycle(["bedwars with Scandlex", "Minecraft", "Anime SMP", "Hypixel", "with AcidicBlaster", "Skywars", "BlocksMC", "Competetive Cracked Bedwars"])
-status_b = cycle(["Hentai", "Anime", "YouTube", "AcidicBlaster", "RayVene", "F1", f"over {len(client.guilds)} servers"])
+status_a = cycle(["bedwars with Scandlex", "Minecraft", "Survival", "Skywars",])
+status_b = cycle(["Anime", "YouTube", "F1", f"over {len(client.fetch_guilds())} servers"])
 
 #Changing Statuses
 
@@ -35,16 +35,6 @@ client.loop.create_task(status())
 
 #Errors
 
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-       await ctx.send("wadre")
-
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You do not have permission to run this command!")
-
 #Logging
 
 @client.event
@@ -59,28 +49,49 @@ async def on_guild_remove(ctx, member):
 
 #Commands
 
-@client.command()
+@client.command(name = "ping")
 async def ping(ctx):
-    await ctx.send(f"Pong!\n\nThe Latency is {round(client.latency * 1000)}ms.")
-
-@client.command(aliases = ["dice"])
-async def roll(ctx):
-    choices = [1, 2, 3 , 4, 5, 6]
-    number = random.choice(choices)
-    embed = discord.Embed(name = "Dice", description = f"You have rolled {number}", color = discord.Color.dark_gray())
+    embed = discord.Embed(name = "Ping", description = f"The Latency is {round(client.latency * 1000)}ms!", color = discord.Color.dark_gray)
     await ctx.send(embed = embed)
 
 @client.command(name = "userinfo", aliases = ["whois"])
-async def user(ctx, member : discord.Member):
-    embed = discord.Embed(name = member.name, description = member.mention, color = discord.Color.dark_gray())
+async def user(ctx, *, member : discord.Member = None):
+    member = ctx.author if not member else member
+    roles = [role for role in member.roles]
+    embed = discord.Embed(name = member.name, description = member.mention, color = discord.Color.dark_gray(), timestamp = ctx.message.created_at)
     embed.add_field(name = "ID:", value = member.id, inline = False)
     embed.add_field(name = "Joined Discord:", value = member.created_at.strftime("%a, %d %b %Y %I:%M %p"), inline = False)
     embed.add_field(name = "Joined Server:", value = member.joined_at.strftime("%a, %d %b %Y %I:%M %p"), inline = False)
+    embed.add_field(name = "Roles:", value = " ".join([role.mention for role in roles]), inline = False)
     embed.add_field(name = "Bot:", value = member.bot, inline = False)
 
     embed.set_thumbnail(url = member.avatar_url)
     embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
     await ctx.send(embed = embed)
+
+@client.comamnd(name = "avatar", aliases = ["av"])
+async def avatar(ctx, member : discord.Member = None):
+    member = ctx.author if not member else member
+    embed = discord.Embed(name = member.name, color = discord.color.dark_gray(), timestamp = ctx.message.created_at)
+    embed.set_image(url = member.avatar_url)
+    embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
+    await ctx.send(embed = embed)
+
+@client.command(name = "serverinfo", aliases = ["guildinfo"])
+async def serverinfo(ctx):
+    embed = discord.Embed(title = f"{ctx.guild.name} Server Information", color = discord.color.dark_gray(), timestamp = ctx.message.created_at)
+    embed.add_field(name = "Owner:", value = ctx.guild.owner, inline = True)
+    embed.add_field(name = "Server ID:", value = ctx.guild.id, inline = True)
+    embed.add_field(name = "Region:", value = ctx.guild.region, inline = True)
+    embed.add_field(name = "Created on:", value = ctx.guild.created_at.strftime("%a, %d %b %Y %I:%M %p"), inline = True)
+    embed.add_field(name = "Total Members:", value = ctx.guild.member_count, inline = True)
+    embed.add_field(name = "Total Roles:", value = ctx.guild.role_count, inline = True)
+    
+    embed.set_thumbnail(url = ctx.guild.icon_url)
+    embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
+    await ctx.send(embed = embed)
+    
+
 
 
 #Import cog
@@ -91,4 +102,3 @@ for filename in os.listdir("./cogs"):
         print(f"{filename} has loaded.")
 
 client.run("NzkyNjcxNDkwMTUxNjc3OTYy.X-hG2g.HUyrILSMzZVY1IDIqHetw8ecj-w")
-
