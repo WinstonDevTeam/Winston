dc9346a07a3cb979063a96b9c010df648f2a2316
Scandlex <63729658+Scandlex@users.noreply.github.com>
2021-01-23 14:09:17 +0400

    color mistake big brain

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ async def user(ctx, *, member : discord.Member = None):
     embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
     await ctx.send(embed = embed)
 
-@client.command(name = "avatar", aliases = ["av"])
+@client.comamnd(name = "avatar", aliases = ["av"])
 async def avatar(ctx, member : discord.Member = None):
     member = ctx.author if not member else member
-    embed = discord.Embed(name = member.name, color = discord.color.dark_gray(), timestamp = ctx.message.created_at)
+    embed = discord.Embed(name = member.name, color = discord.Color.dark_gray(), timestamp = ctx.message.created_at)
     embed.set_image(url = member.avatar_url)
     embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
     await ctx.send(embed = embed)
