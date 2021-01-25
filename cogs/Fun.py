d26381ea9ca378952fbabe6c11bef386855fac60
Scandlex <63729658+Scandlex@users.noreply.github.com>
2021-01-25 22:06:07 +0400

    added reddit api

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -3,6 +3,9 @@ import asyncio
 from discord.ext import commands
 from discord.ext.commands import bot
 import random
+import praw
+
+reddit = praw.Reddit(client_id = "oqxjNa18tQU_mQ", client_secret = "QmkRGc5Vl1L1ocqWbQ_zsXqsP7S0LA", username = "PythonAPIBot", password = "chrisv23@minecraft", user_agent = "Winston")
 
 class Fun(commands.Cog):
     def __init__(self, client):
@@ -14,6 +17,28 @@ class Fun(commands.Cog):
         number = random.choice(choices)
         embed = discord.Embed(name = "Dice", description = f"You have rolled {number}", color = discord.Color.dark_gray())
         await ctx.send(embed = embed)
+
+    @commands.command(name = "meme", subreddit = "dankmemes")
+    async def meme(self, ctx):
+        subreddit = reddit.subreddit("formuladank", "dankmemes", "titanfolk", "animememes", "shitpostcrusaders", "196", "cricketshitposts")
+        all_subs = []
+
+        top = subreddit.top(limit = 50)
+
+        for submission in top:
+            all_subs.append(submission)
+
+        choice_sub = random.choice(all_subs)
+        
+        name = choice_sub.title
+        url = choice_sub.url
+
+        embed = discord.Embed(name = name)
+        embed.set_image(url = url)
+
+        await ctx.send(embed = embed)
+
+
 
 
 def setup(client):
