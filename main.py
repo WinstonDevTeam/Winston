b76fd1935f83b6d2de4a9bddfc8c2893ee642c59
Technoselbow <dhruvbhatia.2212@gmail.com>
2021-02-21 20:52:31 +0400

    Adding custom prefix

--- main.py
+++ main.py
@@ -12,16 +12,63 @@ from itertools import cycle
 
 token = os.environ["TOKEN"]
 
+#Custom Prefix
+def get_prefix(client, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    
+    return prefixes[str(message.guild.id)]
+
+
 #Intents
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = "/", case_insensitive = True, intents = intents)
+
+
+#Custom prefix
+
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = "/"
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.event
+async def changeprefix(ctx, prefix):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+    await ctx.send(f"The prefix has been changed to {prefix}")
+
+
 
 #Status Loop
 
 server_count = len(client.guilds)
 status_a = cycle(["bedwars with Scandlex", "Minecraft", "Survival", "Skywars",])
 status_b = cycle(["Anime", "YouTube", "F1"])
+
+#Changing Statuses
 
 async def status():
     while True:
@@ -33,13 +80,14 @@ async def status():
         await client.change_presence(status = discord.Status.idle, activity = discord.Activity(type = discord.ActivityType.listening, name = "/help"))
         await asyncio.sleep(10)
 
-supporters = [773893391485370378, 400355889187389450, 400857098121904149, 635019218991054848, 254219520980287489, 733532987794128897]
-THEME = discord.Color.dark_gray()
-
 @client.event
 async def on_ready():
     print("Winston is now Online.")
 client.loop.create_task(status())
+
+supporters = [773893391485370378, 400355889187389450, 400857098121904149, 635019218991054848, 254219520980287489, 733532987794128897]
+
+#Errors
 
 #Logging
 
@@ -51,18 +99,62 @@ async def on_guild_join(guild):
 @client.event
 async def on_guild_remove(guild):
     log_channel = client.get_channel(796677487823945728)
-    await log_channel.send(f"Winston left **{guild}**.\nOwner: **{guild.owner}**\nOwner ID: **{guild.owner.id}**")
+    await log_channel.send(f"Winston left {guild}.\nOwner: {guild.owner}\nOwner ID: {guild.owner.id}")
+
+#Commands
+
+@client.command(name = "ping")
+async def ping(ctx):
+    embed = discord.Embed(name = "Ping", description = f"The Latency is {round(client.latency * 1000)}ms!", color = discord.Color.dark_gray())
+    await ctx.send(embed = embed)
+
+@client.command(name = "userinfo", aliases = ["whois"])
+async def user(ctx, *, member : discord.Member = None):
+    member = ctx.author if not member else member
+    roles = [role for role in member.roles if role != ctx.guild.default_role]
+    embed = discord.Embed(name = member.name, description = member.mention, color = discord.Color.dark_gray(), timestamp = ctx.message.created_at)
+    embed.add_field(name = "ID:", value = member.id, inline = False)
+    embed.add_field(name = "Joined Discord:", value = member.created_at.strftime("%a, %d %b %Y %I:%M %p"), inline = False)
+    embed.add_field(name = "Joined Server:", value = member.joined_at.strftime("%a, %d %b %Y %I:%M %p"), inline = False)
+    embed.add_field(name = "Roles:", value = " ".join([role.mention for role in roles]), inline = False)
+    embed.add_field(name = "Bot:", value = member.bot, inline = False)
+
+    embed.set_thumbnail(url = member.avatar_url)
+    embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
+    await ctx.send(embed = embed)
+
+@client.command(name = "avatar", aliases = ["av"])
+async def avatar(ctx, member : discord.Member = None):
+    member = ctx.author if not member else member
+    embed = discord.Embed(title = f"{member}", color = discord.Color.dark_gray(), timestamp = ctx.message.created_at)
+    embed.set_image(url = member.avatar_url)
+    embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
+    await ctx.send(embed = embed)
+
+@client.command(name = "serverinfo", aliases = ["guildinfo"])
+async def serverinfo(ctx):
+    embed = discord.Embed(title = f"{ctx.guild.name} Server Information", description = ctx.guild.description, color = discord.Color.dark_gray(), timestamp = ctx.message.created_at)
+    embed.add_field(name = "Owner:", value = ctx.guild.owner, inline = True)
+    embed.add_field(name = "Server ID:", value = ctx.guild.id, inline = True)
+    embed.add_field(name = "Region:", value = ctx.guild.region, inline = True)
+    embed.add_field(name = "Total Members:", value = ctx.guild.member_count, inline = True)
+    
+    embed.set_thumbnail(url = ctx.guild.icon_url)
+    embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Requested by {ctx.author.name}")
+    await ctx.send(embed = embed)
 
 @client.command(name = "invite")
 async def invite(ctx):
     invite_url = "https://discord.com/api/oauth2/authorize?client_id=792671490151677962&permissions=8&scope=bot"
     embed = discord.Embed(
-        title = "Click here to invite Winston!", url = invite_url, color = THEME)
+        title = "Click here to invite Winston!", url = xinvite_url, color = discord.Color.dark_gray())
     await ctx.send(embed = embed)
 
 @client.command(name = "support")
 async def support(ctx):
-    await ctx.send("Support Server:\nhttps://discord.gg/ASw7eUBssP")
+    await ctx.send("Support Server:\nhttps://discord.gg/Cy8UA5va")
+    
+#Import cog
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
